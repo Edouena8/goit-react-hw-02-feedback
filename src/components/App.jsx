38bdc7710ca0804e05bf4981c6929b6ca2630414
Edouena8 <103317047+Edouena8@users.noreply.github.com>
2023-03-14 17:29:11 +0100

    finished task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
+import React from "react";
+import { Component } from "react";
+import FeedbackOptions from "./FeedbackOptions";
+import Statistics from "./Statistics";
+import Section from "./Section";
+import Notification from "./Notification";
+
+class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = value => {
+    this.setState(prevState => ({
+      [value]: prevState[value] + 1,
+    }));
+  };
+
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((value, acc) => acc += value, 0);
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(this.state.good * 100 / this.countTotalFeedback() || 0);
+  }
+
+  render() {
+    const {good, neutral, bad} = this.state;
+    return (
+      <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+        display: 'flex-start',
+        padding: '40px',
+        flexDirection: 'column',
+        gap: '40px',
+        color: '#010101',
+      }}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions 
+            options={Object.keys(this.state)} 
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+
+        <Section title='Statistics'>
+          {!this.countTotalFeedback() ? (
+            <Notification message="There is no feedback"/>
+            ) : (
+            <Statistics 
+              good={good} 
+              neutral={neutral} 
+              bad={bad} 
+              total={this.countTotalFeedback()} 
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+            )
+          }
+        </Section>
+      </div>
+    )
+  }
+}
+
+export default App;
